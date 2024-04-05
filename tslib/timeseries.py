7df3788f516b449a6f87ddcff494a46fdba6bab6
Emi <Emanuel.boehm@gmx.de>
2024-04-05 16:11:46 +0200

    use clearer naming convention when creating a time series. Also improve doc strings

--- tslib/timeseries.py
+++ tslib/timeseries.py
@@ -2,12 +2,6 @@
 Timeseries
 ----------
 ``Timeseries`` is the main representation of data in this library.
-
-It contains of:
-data:
-    The `DataFrame` containing the relevant data.
-indices:
-    time_indices used for the time series.
 """
 
 import pandas as pd
@@ -19,38 +13,37 @@ DEFAULT_TIME_COLUMN = "time"
 
 # The timeseries may also consist of following private fields
 # self._time_column: [str | None] indicating the column of the indices in the df
-# self._index_type: "integer" | "datetime" depening on the type of indices
 class Timeseries:
-    def __init__(self, data, time_column: str | int = DEFAULT_TIME_COLUMN):
+    def __init__(self, df, time_column: str | int = DEFAULT_TIME_COLUMN):
         """
         Creates a new ``Timeseries`` object.
-        The data has to be or be convertable to a DataFrame (:class:`pandas.DataFrame`).
+        The df has to be or be convertable to a DataFrame (:class:`pandas.DataFrame`).
 
         Parameters
         ----------
-        data
-            The DataFrame or a structure that can be converted as such.
+        df
+            The `DataFrame` or a structure that can be converted as such.
         time_column
             The name of the column used as "time" column.
             The column itself should be a subclass of :class:`np.integer`, :class:`float` or :class: `np.datetime64`.
         """
-        if not isinstance(data, pd.DataFrame):
+        if not isinstance(df, pd.DataFrame):
             utils.warn(
                 "Data for `Timeseries` should be of type `DataFrame`. Trying to convert to `DataFrame`."
             )
             try:
-                data = pd.DataFrame(data)
+                df = pd.DataFrame(df)
             except Exception as conversion_exception:
                 raise conversion_exception
         if time_column:
             if isinstance(time_column, int):
                 # The user has the responsibility to provide a valid index for the dataframe
                 assert (
-                    0 <= time_column < len(data.columns)
+                    0 <= time_column < len(df.columns)
                 ), "Couldn't index `DataFrame` by `time_column`"
-                time_column = str(data.columns[time_column])
+                time_column = str(df.columns[time_column])
             elif isinstance(time_column, str):
-                if not time_column in data.columns:
+                if not time_column in df.columns:
                     raise IndexError(
                         f"Column '{time_column}' not found in `DataFrame`."
                     )
@@ -60,7 +53,7 @@ class Timeseries:
                                   but is of type {type(time_column)}."
                 )
             self._time_column = time_column
-        self.data = data
+        self.df = df
 
     def validate(self):
         """
@@ -83,11 +76,11 @@ class Timeseries:
         """
         # Check if time_column does exist in data frame.
         # It may be removed after creation
-        if self._time_column not in self.data.columns:
+        if self._time_column not in self.df.columns:
             raise IndexError(
-                f"Validation failed. Column '{self._time_column}' not found in `self.data`.",
+                f"Validation failed. Column '{self._time_column}' not found in `self.df`.",
             )
-        time_type = self.data.dtypes[self._time_column]
+        time_type = self.df.dtypes[self._time_column]
 
         # Check if the time column contains a valid datatype.
         if (
@@ -98,7 +91,7 @@ class Timeseries:
             raise AttributeError(
                 "Invalid type of column referenced by `time_column`. Should be of type :class:`np.integer`, :class:`float` or :class:`np.datetime64`."
             )
-        time_values = self.data[self._time_column]
+        time_values = self.df[self._time_column]
         if any(
             time_values[i] >= time_values[i + 1] for i in range(len(time_values) - 1)
         ):
