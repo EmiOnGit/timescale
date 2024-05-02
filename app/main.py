f2797628523db4b261705a0d868804826ede9ca5
Emi <Emanuel.boehm@gmx.de>
2024-05-02 18:30:59 +0200

    cleanup info board, implement automatic alignment inside the ui directly based on selected parameters in `info_board`

--- app/main.py
+++ app/main.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python
+from bayes_opt import BayesianOptimization
 import plotly.graph_objects as go
 import pyarrow as pa
 import pyarrow.parquet as pq
-import os, sys
+from dash import Dash, State, html, dcc, callback, Output, Input
+import json
+import dash_bootstrap_components as dbc
 
-from app.state import State
+import pandas as pd
+import numpy as np
+from app.state import Alignment, Settings, ViewState, alignment_or_default
+import base64
 
+import os
+import sys
 
 pathname = os.path.dirname(sys.argv[0])
 path = pathname + "/.."
 # needed to import tslib when executing this file
 sys.path.append(path)
-import pandas as pd
-from dash import Dash, html, dcc, callback, Output, Input
-import dash_bootstrap_components as dbc
-from tslib.processing.pipeline import *
+from tslib.processing.pipeline import Pipeline, add, interpolate, index_to_time
 from tslib.generator import generate
+import tslib.processing.alignment as tsalign
 import tslib.io as tio
-from ts_view import TsView
-from info_board import InfoBoard
-import numpy as np
 
 FACTOR = 2
 OFFSET = 10
@@ -33,7 +36,6 @@ timeseries_block = [
     dbc.Col(
         [
             html.H1(f"ts{i}", style={"textAlign": "center"}),
-            html.Div(id=f"info_n{i}", children="n="),
             dcc.Upload(
                 id=f"info_upload{i}",
                 children=html.Div([f"Upload for ts{i}"]),
@@ -44,18 +46,72 @@ timeseries_block = [
                     "borderStyle": "dashed",
                     "borderRadius": "5px",
                     "textAlign": "center",
-                    "margin": "10px",
+                    "margin-right": "70px",
+                    "margin-left": "70px",
                 },
                 multiple=False,
             ),
-        ]
+            html.Div(id=f"info_n{i}", children="n="),
+        ],
+        style={
+            "display": "flex",
+            "flex-direction": "column",
+            "align-items": "center",
+            "justify-content": "center",
+        },
     )
-    for i in range(2)
+    for i in range(1, 3)
 ]
+settings_block = dbc.Col(
+    [
+        html.H1("Settings", style={"textAlign": "center"}),
+        dcc.Dropdown(
+            ["correlation", "function sum"],
+            "correlation",
+            id="method_dropdown",
+            style={
+                "width": "220px",
+                "textAlign": "center",
+                "margin-bottom": "10px",
+            },
+        ),
+        html.Div(
+            [
+                html.Div("points"),
+                dcc.Slider(
+                    id="slider_points",
+                    min=1,
+                    max=500,
+                    step=1,
+                    value=100,
+                    marks={i: "{}".format(i) for i in range(0, 501, 100)},
+                    # style={"width": "200px"},
+                ),
+                html.Div("iterations"),
+                dcc.Slider(
+                    id="slider_iterations",
+                    min=1,
+                    max=80,
+                    step=1,
+                    value=16,
+                    marks={i: "{}".format(i) for i in range(0, 81, 10)},
+                ),
+            ],
+            style={"width": "300px"},
+        ),
+        html.Button("Align", id="calculate_align"),
+    ],
+    style={
+        "display": "flex",
+        "flex-direction": "column",
+        "align-items": "center",
+        "justify-content": "center",
+    },
+)
 app.layout = html.Div(
     [
         html.H1(children="TimeScale", style={"textAlign": "center"}),
-        dbc.Row([timeseries_block[0], timeseries_block[1]]),
+        dbc.Row([timeseries_block[0], timeseries_block[1], settings_block]),
         html.Div(id="correlation", style={"whiteSpace": "pre-line"}),
         dcc.Graph(id="graph-content"),
         html.Plaintext(children="scale", style={"textAlign": "center"}),
@@ -82,15 +138,60 @@ app.layout = html.Div(
         ),
         dcc.Store(id="ts1store"),
         dcc.Store(id="ts2store"),
+        dcc.Store(id="alignment"),
+        dcc.Store(id="settings"),
     ]
 )
 
-import base64
+
+@callback(
+    Output("alignment", "data", allow_duplicate=True),
+    Input("calculate_align", "n_clicks"),
+    State("ts1store", "data"),
+    State("ts2store", "data"),
+    State("settings", "data"),
+    prevent_initial_call=True,
+)
+def align(clicks, ts1json, ts2json, settings):
+    del clicks
+    ts1 = tio.from_json(ts1json)
+    ts2 = tio.from_json(ts2json)
+    settings = Settings(**json.loads(settings))
+    pbounds = {"translation": (-200, 200), "scale": (0.2, 5.0)}
+    optimizer = BayesianOptimization(
+        f=tsalign.aligner(ts1, ts2),
+        pbounds=pbounds,
+        verbose=1,  # verbose = 1 prints only when a maximum is observed, verbose = 0 is silent
+    )
+    optimizer.maximize(
+        init_points=settings.points,
+        n_iter=settings.iterations,
+    )
+    print("finished alignment search")
+    best = optimizer.max["params"]
+    if best is None:
+        return alignment_or_default(None)
+    else:
+        scale = best["scale"]
+        offset = int(best["translation"])
+        return json.dumps(Alignment(scale, offset).__dict__)
+
+
+@callback(
+    Output("settings", "data"),
+    Input("slider_points", "value"),
+    Input("slider_iterations", "value"),
+    Input("method_dropdown", "value"),
+)
+def register_settings(points, iterations, align_method):
+    return json.dumps(
+        Settings(align_method, points=points, iterations=iterations).__dict__
+    )
 
 
 @callback(
     Output("ts1store", "data"),
-    Input("info_upload0", "contents"),
+    Input("info_upload1", "contents"),
 )
 def register_upload(content):
     if content is None:
@@ -107,7 +208,7 @@ def register_upload(content):
 
 @callback(
     Output("ts2store", "data"),
-    Input("info_upload1", "contents"),
+    Input("info_upload2", "contents"),
 )
 def register_upload2(content):
     if content is None:
@@ -122,29 +223,41 @@ def register_upload2(content):
     return tio.to_json(ts)
 
 
-@callback(Output("info_n0", "children"), Input("ts1store", "data"))
-def on_ts_change(ts1json):
+@callback(Output("info_n1", "children"), Input("ts1store", "data"))
+def info_ts1(ts1json):
     ts1 = tio.from_json(ts1json)
     return f"n: {len(ts1.df)}"
 
 
-@callback(Output("info_n1", "children"), Input("ts2store", "data"))
-def on_ts2_change(ts2json):
+@callback(Output("info_n2", "children"), Input("ts2store", "data"))
+def info_ts2(ts2json):
     ts2 = tio.from_json(ts2json)
     return f"n: {len(ts2.df)}"
+
+
+@callback(
+    Output("alignment", "data", allow_duplicate=True),
+    Input("slider_scale", "value"),
+    Input("slider_offset", "value"),
+    prevent_initial_call=True,
+)
+def alignment_slider(scale, offset):
+    return json.dumps(Alignment(scale, offset).__dict__)
 
 
 @callback(
     Output("correlation", "children"),
     Input("ts1store", "data"),
     Input("ts2store", "data"),
-    Input("slider_scale", "value"),
-    Input("slider_offset", "value"),
+    Input("alignment", "data"),
+    Input("settings", "data"),
 )
-def update_correlation(ts1, ts2, scale, offset):
+def update_correlation(ts1, ts2, alignment, settings):
     ts1, ts2 = tio.from_json(ts1), tio.from_json(ts2)
-    state = State(ts1, ts2, scale, offset)
-    correlations = state.correlations()
+    settings = Settings(**json.loads(settings))
+    alignment = alignment_or_default(alignment)
+    state = ViewState(ts1, ts2, alignment)
+    correlations = state.apply(settings.align_method)
     sum = np.sum(correlations["corr"])
     return f"Correlation: {sum}"
 
@@ -153,17 +266,22 @@ def update_correlation(ts1, ts2, scale, offset):
     Output("graph-content", "figure"),
     Input("ts1store", "data"),
     Input("ts2store", "data"),
-    Input("slider_scale", "value"),
-    Input("slider_offset", "value"),
+    Input("alignment", "data"),
+    Input("settings", "data"),
 )
-def update_graph(ts1, ts2, scale, offset):
+def update_graph(ts1, ts2, alignment, settings):
     ts1, ts2 = tio.from_json(ts1), tio.from_json(ts2)
-    state = State(ts1, ts2, scale, offset)
+    alignment = alignment_or_default(alignment)
+    settings = Settings(**json.loads(settings))
+    state = ViewState(ts1, ts2, alignment)
     ts2_trans = state.transform_ts2()
-    correlations = state.correlations()
+    correlations = state.apply(settings.align_method)
     fig = go.Figure()
-    fig.add_scatter(x=ts2_trans.df["timestamp"], y=ts2_trans.df["value-0"])
-    fig.add_scatter(x=ts1.df["timestamp"], y=ts1.df["value-0"])
+    fig.layout.__setattr__("uirevision", "const")
+    fig.add_scatter(x=ts2_trans.df["timestamp"], y=ts2_trans.df["value-0"], name="ts2")
+    # if 'uirevision' stays as it was before updating the figure, the zoom/ui will not reset. 'const' as value is arbitrary
+    fig.add_scatter(x=ts1.df["timestamp"], y=ts1.df["value-0"], name="ts1")
+
     fig.add_bar(x=correlations["timestamp"], y=correlations["corr"], name="correlation")
     return fig
 
@@ -179,28 +297,11 @@ def default_ts1():
 def default_ts2():
     ts2 = generate.generate_simple_with_noise(n=100, dimensions=1)
     pipeline = Pipeline()
-    pipeline.push(add(1))
+    pipeline.push(add(4))
     ts2 = pipeline.apply(ts2)
     ts2.df = pd.DataFrame(ts2.df[OFFSET:]).dropna(axis="rows")
     return ts2
 
 
-def init_data():
-    ts = generate.generate_simple_with_noise(n=100, dimensions=1)
-    ts2 = generate.generate_simple_with_noise(n=100, dimensions=1)
-    pipeline = Pipeline()
-    pipeline.push(interpolate(factor=FACTOR)).push(index_to_time)
-    ts = pipeline.apply(ts)
-    pipeline = Pipeline()
-    # lazy
-    pipeline.push(add(1))
-    ts2 = pipeline.apply(ts2)
-    ts2.df = pd.DataFrame(ts2.df[OFFSET:]).dropna(axis="rows")
-    ts_view = TsView()
-    info_board = InfoBoard()
-    return ts_view, info_board
-
-
 if __name__ == "__main__":
-    ts_view, info_board = init_data()
     app.run(debug=True)
--- app/state.py
+++ app/state.py
@@ -9,62 +9,65 @@ import numpy as np
 import pandas as pd
 
 from tslib.timeseries import Timeseries
+import json
 
 
-# class State:
-#     def __init__(self, ts1, ts2):
-#         self.ts1 = ts1
-#         self.ts2 = ts2
-#         self.scale = 1.0
-#         self.offset = 0
-#         self.cached = ts2
-
-#         self.norm_pipeline = Pipeline().push(normalization(-1.0, 1.0))
-#         self.ts1_normalized = self.norm_pipeline.apply(ts1)
-
-#     def transform_ts2(self, scale, offset):
-#         if scale == self.scale and offset == self.offset:
-#             return self.cached
-#         self.scale = scale
-#         self.offset = offset
-
-#         pipeline = Pipeline()
-#         pipeline.push(interpolate(factor=self.scale)).push(index_to_time)
-#         ts_trans = pipeline.apply(self.ts2)
-#         ts_trans.df[ts_trans._time_column] = [
-#             x + self.offset for x in ts_trans.time_column()
-#         ]
-#         self.cached = ts_trans
-#         return self.cached
-
-
-#     def get_corr(self):
-#         return calculate_corr(
-#             self.ts1_normalized, self.norm_pipeline.apply(self.cached)
-#         )
 @dataclass
-class State:
-    ts1: Timeseries
-    ts2: Timeseries
+class Settings:
+    align_method: str
+    points: int
+    iterations: int
+
+
+def alignment_or_default(input):
+    if input is None:
+        return Alignment(scale=1.0, offset=0)
+    else:
+        return Alignment(**json.loads(input))
+
+
+@dataclass
+class Alignment:
     scale: float
     offset: int
 
+
+@dataclass
+class ViewState:
+    ts1: Timeseries
+    ts2: Timeseries
+    alignment: Alignment
+
     def transform_ts2(self):
         pipeline = Pipeline()
-        pipeline.push(interpolate(factor=self.scale)).push(index_to_time)
+        pipeline.push(interpolate(factor=self.alignment.scale)).push(index_to_time)
         ts_trans2 = pipeline.apply(self.ts2)
         ts_trans2.df[ts_trans2._time_column] = [
-            x + self.offset for x in ts_trans2.time_column()
+            x + self.alignment.offset for x in ts_trans2.time_column()
         ]
         norm_pipeline = Pipeline().push(normalization(-1.0, 1.0))
         ts_trans2 = norm_pipeline.apply(ts_trans2)
         return ts_trans2
 
-    def correlations(self):
+    def apply(self, align_method):
         ts_trans2 = self.transform_ts2()
         norm_pipeline = Pipeline().push(normalization(-1.0, 1.0))
         ts1_normalized = norm_pipeline.apply(self.ts1)
-        return calculate_corr(ts1_normalized, ts_trans2)
+        if align_method == "correlation":
+            return calculate_corr(ts1_normalized, ts_trans2)
+        elif align_method == "function sum":
+            return calculate_sum(ts1_normalized, ts_trans2)
+        else:
+            print(f"couldn't match align method: {align_method}")
+            return pd.DataFrame({"corr": [0]})
+
+
+def calculate_sum(ts1, ts2):
+    df = pd.merge(ts1.df, ts2.df, on="timestamp")
+    df["corr"] = df.loc[:, df.columns != "timestamp"].apply(
+        lambda x: np.abs(np.sum(x)), axis=1
+    )
+    return df
 
 
 def calculate_corr(ts1, ts2):
