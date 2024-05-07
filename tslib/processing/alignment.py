d7f6623a482ebbea6339fd399bcc23c526ae1922
Emi <Emanuel.boehm@gmx.de>
2024-05-07 11:12:26 +0200

    add testing for horizontal translation of timeseries. Alignment.offset -> Alignment.translation

--- tslib/processing/alignment.py
+++ tslib/processing/alignment.py
@@ -1,13 +1,12 @@
-import pandas as pd
-import numpy as np
 from tslib.processing.pipeline import (
     Pipeline,
     normalization,
     interpolate,
     index_to_time,
 )
+import pandas as pd
+import numpy as np
 
-# from bayes_opt import BayesianOptimization
 from plotly.graph_objects import Figure
 from abc import ABC, abstractmethod
 from tslib.timeseries import Timeseries
@@ -17,17 +16,29 @@ from dataclasses import dataclass
 @dataclass
 class Alignment:
     scale: float
-    offset: float
+    translation: float
 
 
 def align(ts1, ts2, alignerclass):
     def inner(translation: float, scale: float):
-        alignment = Alignment(scale, offset=translation)
+        alignment = Alignment(scale, translation)
         aligner = alignerclass(ts1, ts2)
         aligner.transform(alignment)
         return aligner.alignment_score()
 
     return inner
+
+
+def translate(ts: Timeseries, translation: float):
+    ts.df[ts._time_column] = [x + int(translation) for x in ts.time_column()]
+    if not translation.is_integer():
+        lam = translation - int(translation)
+        data_df = ts.data_df()
+        for c in data_df:
+            x = data_df[c].to_numpy()
+            one_shift = pd.Series(x[1:]).to_numpy()
+            one_shift = np.append(one_shift, x[-1])
+            ts.df[c] = (1.0 - lam) * x + lam * one_shift
 
 
 @dataclass
@@ -36,28 +47,13 @@ class BaseAligner(ABC):
     ts2: Timeseries
 
     def transform(self, alignment: Alignment):
+        norm_pipeline = Pipeline().push(normalization(-1.0, 1.0))
         pipeline = Pipeline()
         pipeline.push(interpolate(factor=alignment.scale)).push(index_to_time)
         ts_trans2 = pipeline.apply(self.ts2)
-        ts_trans2.df[ts_trans2._time_column] = [
-            x + int(alignment.offset) for x in ts_trans2.time_column()
-        ]
-        if not alignment.offset.is_integer():
-            lam = alignment.offset - int(alignment.offset)
-            data_df = ts_trans2.data_df()
-            for c in data_df:
-                x = data_df[c]
-                one_shift = pd.Series(x[1:]).to_numpy()
-                ts_trans2.df[c] = (1.0 - lam) * x + lam * np.append(
-                    one_shift, x.values[-1]
-                )
-
-        norm_pipeline = Pipeline().push(normalization(-1.0, 1.0))
-        ts_trans2 = norm_pipeline.apply(ts_trans2)
-        norm_pipeline = Pipeline().push(normalization(-1.0, 1.0))
-        ts1_normalized = norm_pipeline.apply(self.ts1)
-        self.ts1 = ts1_normalized
-        self.ts2 = ts_trans2
+        translate(ts_trans2, alignment.translation)
+        self.ts2 = norm_pipeline.apply(ts_trans2)
+        self.ts1 = norm_pipeline.apply(self.ts1)
 
     @abstractmethod
     def alignment_score(self) -> float:
